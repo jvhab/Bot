16acd00d1c8e5f4ba4e22d53a5e78543eae2c318
Basyrov Konstantin <jvhab@yandex.ru>
2022-12-29 10:29:27 +0500

    Initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			switch update.Message.Command() {
+			case "help":
+				helpCommand(bot, update.Message)
+			case "list":
+				listCommand(bot, update.Message)
+			default:
+				defaultBehavior(bot, update.Message)
+			}
+		}
+	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help\n"+"/list - list products")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "TBD")
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+	bot.Send(msg)
+}
